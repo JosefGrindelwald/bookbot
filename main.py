429bc97edf2915382c96ca3063ee315e79ee7b09
JosefGrindelwald <james.t.nemo@gmail.com>
2025-12-14 23:39:16 +0100

    D: Test Funktion fix ?

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ def get_book_text(path_to_file):
   with open(path_to_file) as f:
     file_contents = f.read()
     return file_contents 
-return get_book_text(./frankenstein.txt)
+return get_book_text(frankenstein.txt)
