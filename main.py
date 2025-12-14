9ecb4bd772b02ff2550aed18f8eb6065b9164483
JosefGrindelwald <james.t.nemo@gmail.com>
2025-12-14 23:37:55 +0100

    C: Test Funktion

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+  with open(path_to_file) as f:
+    file_contents = f.read()
+    return file_contents 
+return get_book_text(./frankenstein.txt)
