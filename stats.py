4e680b968dc5e3ee75c8883265173997d768ab2f
JosefGrindelwald <james.t.nemo@gmail.com>
2025-12-15 00:59:52 +0100

    Update stats.py

--- stats.py
+++ stats.py
@@ -7,7 +7,7 @@ def nummber_of_words(path_to_file):
   words = path_to_file.split()
   num_words = len(words)
   return f"Found {num_words} total words"
-def num_of_character(text):
+def num_of_character(path_to_file):
   characters = {}
   for i in text.lower():
     if i.isalpha():
