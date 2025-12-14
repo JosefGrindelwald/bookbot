531cfe80c9885046db8b629203305a1fc4dce306
JosefGrindelwald <james.t.nemo@gmail.com>
2025-12-15 00:04:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,13 @@ def get_book_text(path_to_file):
     file_contents = f.read()
     return file_contents 
 text = get_book_text("books/frankenstein.txt")
-print(text)
+def nummber_of_words(path_to_file):
+  num_words = 0
+  words = path_to_file.split()
+  for i in range(0, len(words), 1):
+    num_words += 1
+  return f"Found {num_words} total words"
+count = nummber_of_words("books/frankenstein.txt")
+print(count)
+  
+  
