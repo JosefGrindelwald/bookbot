1e000498416f740662ab36199309a716582b0288
JosefGrindelwald <james.t.nemo@gmail.com>
2025-12-15 00:40:38 +0100

    Update stats.py

--- stats.py
+++ stats.py
@@ -7,11 +7,8 @@ def nummber_of_words(path_to_file):
   words = path_to_file.split()
   num_words = len(words)
   return f"Found {num_words} total words"
-def num_of_character(path_to_file):
-  words = path_to_file.split()
-  characters = []
-  for i in range(0, len(words), 1):
-    characters.append(words[i])
-  return len(characters)
+def num_of_characters(text):
+    return len(text)
+  
 
 
