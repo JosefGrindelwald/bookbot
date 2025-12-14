eeef70f60816de848a568b08acb4cf878d241e52
JosefGrindelwald <james.t.nemo@gmail.com>
2025-12-15 00:17:15 +0100

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,1 +1,9 @@
-test
+def get_book_text(path_to_file):
+  with open(path_to_file) as f:
+    file_contents = f.read()
+    return file_contents 
+text = get_book_text("books/frankenstein.txt")
+def nummber_of_words(path_to_file):
+  words = path_to_file.split()
+  num_words = len(words)
+  return f"Found {num_words} total words"
