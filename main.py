a48ff4f9d0a68ec57a598aae3e1375551084e171
JosefGrindelwald <james.t.nemo@gmail.com>
2025-12-15 00:17:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-def get_book_text(path_to_file):
-  with open(path_to_file) as f:
-    file_contents = f.read()
-    return file_contents 
-text = get_book_text("books/frankenstein.txt")
-def nummber_of_words(path_to_file):
-  words = path_to_file.split()
-  num_words = len(words)
-  return f"Found {num_words} total words"
+from stats import get_num_words
 count = nummber_of_words(text)
 print(count)
   
