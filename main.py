3623817401954d91b4c2dd001df4017a2cd9c9dc
JosefGrindelwald <james.t.nemo@gmail.com>
2025-12-16 18:49:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,22 @@ from stats import (
     sort_characters
 )
 
-
+# 🔹 Argument prüfen
 if len(sys.argv) != 2:
-    print("Usage: python main.py <path_to_book>")
+    print("Usage: python3 main.py <path_to_book>")
     sys.exit(1)
 
 path = sys.argv[1]
 
-
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {path}...")
 
 text = get_book_text(path)
 
+print("----------- Word Count ----------")
 print(f"Found {number_of_words(text)} total words")
 
-
+print("--------- Character Count -------")
 
 characters = num_of_character(text)
 sorted_chars = sort_characters(characters)
@@ -28,5 +30,4 @@ for d in sorted_chars:
     print(f"{d['key']}: {d['value']}")
 
 print("============= END ===============")
-  
   
