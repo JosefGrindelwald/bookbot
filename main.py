af6f1d4bf11919e043a41ceb8abdb88c74c80921
JosefGrindelwald <james.t.nemo@gmail.com>
2025-12-16 18:05:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from stats import num_of_character
 from stats import nummber_of_words
 from stats import text
-from stats import sort_on
+from stats import liste
 count = nummber_of_words(text)
 print(count)
-liste = num_of_character(text)
 liste.sort(key=sort_on, reverse=True)
 print(liste)
      
