7e53c991fa6a4925aa95b767ccd163d02430a470
JosefGrindelwald <james.t.nemo@gmail.com>
2025-12-16 18:41:36 +0100

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,29 +1,29 @@
 def get_book_text(path_to_file):
-  with open(path_to_file) as f:
-    file_contents = f.read()
-    return file_contents 
-text = get_book_text(path_to_file)
-def nummber_of_words(path_to_file):
-  words = path_to_file.split()
-  num_words = len(words)
-  return f"Found {num_words} total words"
-def num_of_character(path_to_file):
-  characters = {}
-  for i in path_to_file.lower():
-    if i.isalpha():
-      if i in characters:
-        characters[i]+= 1
-      else:
-        characters[i] = 1
-  return characters 
-def sort_on(items):
-    return items["value"]
-daten = num_of_character(text)
-liste = [
-    {"key": k, "value": v}
-    for k, v in daten.items()
-]
-liste.sort(key=sort_on, reverse=True)
+    with open(path_to_file, encoding="utf-8") as f:
+        return f.read()
+
+
+def number_of_words(text):
+    return len(text.split())
+
+
+def num_of_character(text):
+    characters = {}
+
+    for c in text.lower():
+        if c.isalpha():
+            characters[c] = characters.get(c, 0) + 1
+
+    return characters
+
+
+def sort_characters(char_dict):
+    liste = [
+        {"key": k, "value": v}
+        for k, v in char_dict.items()
+    ]
+    liste.sort(key=lambda x: x["value"], reverse=True)
+    return liste
 
 
 
