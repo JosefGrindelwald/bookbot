058c794b49d2bca9b91c8c70378d46efb897ccb9
JosefGrindelwald <james.t.nemo@gmail.com>
2025-12-16 18:17:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from stats import nummber_of_words
 from stats import text
 from stats import liste
 from stats import sort_on
+print("============ BOOKBOT ============")
+print("Analyzing book found at books/frankenstein.txt...")
+print("----------- Word Count ----------")
 count = nummber_of_words(text)
 print(count)
+print("--------- Character Count -------")
 liste.sort(key=sort_on, reverse=True)
 for d in liste:
     print(" : ".join(map(str, d.values())))
