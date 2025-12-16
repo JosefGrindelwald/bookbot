f8bdb8dae0a1dfd2f4672340ba7f33bbccd821dc
JosefGrindelwald <james.t.nemo@gmail.com>
2025-12-16 17:58:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from stats import sort_on
 count = nummber_of_words(text)
 print(count)
 liste = num_of_character(text)
-liste.sorted(reverse=True, key = sort_on)
+liste.sort(key=sort_on, reverse=True)
 print(liste)
      
   
